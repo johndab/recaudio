5860ec8400579f11cf06ee1b7c25e8b89cf3414a
Jan Dąbrowski <jan.dabrowski@ilabo.com.pl>
2019-03-02 16:07:49 +0100

    c

--- wp-content/themes/recaudio/components/nav.php
+++ wp-content/themes/recaudio/components/nav.php
@@ -3,7 +3,11 @@
       <a class="navbar-brand" href="#">
         <img src="<?= get_template_directory_uri() ."/img/logo.png" ?>" style="max-height: 50px" />
       </a>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" 
+        data-target="#navbarMain" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarMain">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
             <a class="nav-link" href="#about" v-smooth-scroll >O nas</a>
@@ -20,12 +24,12 @@
           <li class="nav-item">
             <a class="nav-link" href="#contact" v-smooth-scroll>Kontakt</a>
           </li>
-          <li class="nav-item icon ml-4">
+          <li class="nav-item icon mx-2">
             <a class="nav-link" href="https://www.facebook.com/Rec-Audio-2233027257010342/" >
               <i class="fab fa-facebook" ></i>
             </a>
           </li>
-          <li class="nav-item icon">
+          <li class="nav-item icon mx-2">
             <a class="nav-link" href="https://www.instagram.com/recaudio.pl">
               <i class="fab fa-instagram"></i>
             </a>
--- wp-content/themes/recaudio/components/opinions.php
+++ wp-content/themes/recaudio/components/opinions.php
@@ -3,7 +3,6 @@
     <h1 class="section-title">
       <?php echo apply_filters( 'the_content', get_post(52)->post_title ); ?>
     </h1>
-
     <?php echo apply_filters( 'the_content', get_post(52)->post_content ); ?>
   </div>
 </div>
