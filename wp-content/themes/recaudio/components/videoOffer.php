f9ecc923484318cd4c51f9a47c0d9658cc6adb6d
lukaszarpak <szarpak90@gmail.com>
2020-04-26 22:16:10 +0200

    add new pages

---
+++ wp-content/themes/recaudio/components/videoOffer.php
@@ -0,0 +1,13 @@
+<div id="videoOffer">
+<h1 class="section-title">Oferta video</h1>
+<div class="container d-flex justify-content-center flex-wrap">
+    <img src="<?= get_template_directory_uri() ."/img/logo.png" ?>" style="width: 40%; max-width: 250px; margin: 10px" />
+    <img src="<?= get_template_directory_uri() ."/img/logo.png" ?>" style="width: 40%; max-width: 250px; margin: 10px" />
+    <img src="<?= get_template_directory_uri() ."/img/logo.png" ?>" style="width: 40%; max-width: 250px; margin: 10px" />
+    <img src="<?= get_template_directory_uri() ."/img/logo.png" ?>" style="width: 40%; max-width: 250px; margin: 10px" />
+    <img src="<?= get_template_directory_uri() ."/img/logo.png" ?>" style="width: 40%; max-width: 250px; margin: 10px" />
+    <img src="<?= get_template_directory_uri() ."/img/logo.png" ?>" style="width: 40%; max-width: 250px; margin: 10px" />
+    <img src="<?= get_template_directory_uri() ."/img/logo.png" ?>" style="width: 40%; max-width: 250px; margin: 10px" />
+</div>
+<!-- <p>1 Transmisje online koncertow</p> -->
+</div>
--- wp-content/themes/recaudio/index.php
+++ wp-content/themes/recaudio/index.php
@@ -6,8 +6,9 @@ get_header();
 	<div id="app">
 		<?php get_template_part( 'components/nav' ); ?>
 		<?php get_template_part( 'components/carousel' ); ?>
+		<?php get_template_part( 'components/videoOffer' ); ?>
+		<?php get_template_part( 'components/soundOffer' ); ?>
 		<?php get_template_part( 'components/about' ); ?>
-		<?php get_template_part( 'components/offer' ); ?>
 		<?php get_template_part( 'components/realizations' ); ?>
 		<?php get_template_part( 'components/opinions' ); ?>
 		<?php get_template_part( 'components/contact' ); ?>
